961f2f8f8d6004f8311a1c68b381c1f4789369d1
satwinder6510 <142193859+satwinder6510@users.noreply.github.com>
2025-04-28 13:26:50 +0100

    ch

--- client - Copy/src/components/StatsSection.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-// Empty component per request to remove the Historical Signal Model Performance section
-const StatsSection: React.FC = () => {
-  // Return null instead of a section - this effectively removes the component from the DOM
-  return null;
-};
-
-export default StatsSection;
--- client - Copy/src/hooks/use-location.ts
+++
@@ -1,7 +0,0 @@
-import { useLocation as useLocationContext } from '@/contexts/LocationContext';
-
-export const useLocation = () => {
-  return useLocationContext();
-};
-
-export default useLocation;
